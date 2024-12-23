89d0c36de157c930f573515a02d15e9334e10a74
Abdul Moiz <abdulmoiziphone4@gmail.com>
2024-12-23 20:53:18 +0500

    Added Network Configurations to Docker Files

--- backend/server.js
+++ backend/server.js
@@ -11,28 +11,28 @@ cloudinary.v2.config({
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at port ${process.env.PORT}`);
 });
-const adminData = {
-  firstName: "Abdul",
-  lastName: "Moiz",
-  dob: new Date("07/14/2003"),
-  nic: "6110131842197",
-  email: "abdulmoiziphone4@gmail.com",
-  phone: "03348537793",
-  gender: "Male",
-  password: "abdulmoiz123", // Make sure to change this to a secure password
-  role: "Admin",
-};
 
-// Create and save the admin user
-const createAdminUser = async () => {
-  try {
-    const adminUser = new User(adminData);
-    await adminUser.save();
-    console.log("Admin user created successfully!");
-  } catch (err) {
-    console.error("Error creating admin user:", err);
-  }
-};
+// const adminData = {
+//   firstName: "Abdul",
+//   lastName: "Moiz",
+//   dob: new Date("07/14/2003"),
+//   nic: "6110131842197",
+//   email: "abdulmoiziphone4@gmail.com",
+//   phone: "03348537793",
+//   gender: "Male",
+//   password: "abdulmoiz123", // Make sure to change this to a secure password
+//   role: "Admin",
+// };
 
-// Call the function to create the admin user
-createAdminUser();
+// // Create and save the admin user
+// const createAdminUser = async () => {
+//   try {
+//     const adminUser = new User(adminData);
+//     await adminUser.save();
+//     console.log("Admin user created successfully!");
+//   } catch (err) {
+//     console.error("Error creating admin user:", err);
+//   }
+// };
+
+// createAdminUser();
